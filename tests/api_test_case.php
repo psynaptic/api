4376209d6b082744543aad38e6ac16b5e15dd055
Neil Drumm <drumm@3064.no-reply.drupal.org>
2008-12-26 21:19:50 +0000

    Finish converting the tests to using a real test database.

---
+++ tests/api_test_case.php
@@ -0,0 +1,22 @@
+<?php
+// $Id$
+
+class ApiTestCase extends DrupalWebTestCase {
+  function setUp() {
+    parent::setUp('api', 'job_queue');
+
+    include drupal_get_path('module', 'api') .'/api.admin.inc';
+    include drupal_get_path('module', 'api') .'/parser.inc';
+
+    // Make a branch for sample code.
+    $branch->branch_name = '6';
+    $branch->title = 'Testing 6';
+    $branch->directory = drupal_get_path('module', 'api') .'/tests/sample';
+    api_save_branch($branch);
+
+    // Parse the code.
+    api_update_all_branches();
+    while (job_queue_dequeue()) { }
+  }
+}
+
