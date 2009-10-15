8fb6f5dd5cd9c8ea8620c700f5f0d1944b8fda20
Neil Drumm <drumm@3064.no-reply.drupal.org>
2009-10-15 06:42:35 +0000

    Remove transient api_default_branch variable for #218306.

--- templates/api-defined.tpl.php
+++ templates/api-defined.tpl.php
@@ -1,1 +1,1 @@
-<p class="api-defined"><?php print t('!file, line @start_line', array('!file' => api_file_link($object), '@start_line' => $object->start_line)) ?></p>
+<p class="api-defined"><?php print t('!file, line @start_line', array('!file' => api_file_link($branch, $object), '@start_line' => $object->start_line)) ?></p>
