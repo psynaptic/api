d8be4f23cd81264a284ed08e0eea0b698058cc3a
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-02-01 19:53:21 +0000

    Bring tests into closer sync with reality. @file comments are aparently broken

--- tests/api_test_case.php
+++ tests/api_test_case.php
@@ -3,15 +3,17 @@
 
 class ApiTestCase extends DrupalWebTestCase {
   function setUp() {
-    parent::setUp('api', 'job_queue');
+    parent::setUp('job_queue', 'pgp', 'api');
 
     include drupal_get_path('module', 'api') .'/api.admin.inc';
     include drupal_get_path('module', 'api') .'/parser.inc';
 
     // Make a branch for sample code.
     $branch->type = 'files';
+    $branch->project = 'test';
     $branch->branch_name = '6';
     $branch->title = 'Testing 6';
+    $branch->status = 1;
     $branch->data = array(
       'directories' => drupal_get_path('module', 'api') .'/tests/sample',
     );
@@ -20,5 +22,10 @@ class ApiTestCase extends DrupalWebTestCase {
     // Parse the code.
     api_update_all_branches();
     while (job_queue_dequeue()) { }
+  }
+
+  function getBranch() {
+    $branches = api_get_branches();
+    return reset($branches);
   }
 }
