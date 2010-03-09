7ee420c3993176dbb961f81d5a3160b6e3a1a9e8
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-03-09 07:52:01 +0000

    #710774 unit tests for classes

---
+++ tests/sample/classes.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file Object-oriented tests.
+ */
+
+/**
+ * Sample class.
+ */
+class Sample {
+
+  /**
+   * Metasyntatic member function.
+   */
+  public function foo() {
+  }
+}
