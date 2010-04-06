21a8610b1a7ccb2aa60615d1a9913a324cc53191
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-04-06 05:56:24 +0000

    #762610 allow multi-paragraph @param, @see, and @return

--- tests/sample/sample.php
+++ tests/sample/sample.php
@@ -30,8 +30,15 @@ define('SAMPLE_CONSTANT');
  *
  * @param $parameter
  *   A generic parameter.
+ * @param $complex_parameter
+ *   Information about the $complex_parameter parameter. Example:
+ *   @code
+ *     $complex_parameter = 3;
+ *   @endcode
+ *
+ *   A second paragraph about the $complex_parameter parameter.
  */
-function sample_function($parameter) {
+function sample_function($parameter, $complex_parameter) {
 }
 
 /**
