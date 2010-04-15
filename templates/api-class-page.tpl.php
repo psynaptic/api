2ff37e2cc08edba3f05b07a67cfa917127a45a49
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-04-15 06:38:10 +0000

    #757568 add basic class pages. Looks horrible at the moment, but at least it is a page.

--- templates/api-class-page.tpl.php
+++ templates/api-class-page.tpl.php
@@ -6,7 +6,7 @@
 <?php } ?>
 
 <?php print $defined; ?>
-<?php print $code; ?>
+<?php print theme('ctools_collapsible', t('View code'), $code, $collapsed = TRUE) ?>
 
 <?php if (!empty($related_topics)) { ?>
   <h3><?php print t('Related topics') ?></h3>
