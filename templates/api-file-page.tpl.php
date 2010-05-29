757d223d0077fe1c050f2c0310b4197e72400f37
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-05-29 23:25:27 +0000

    Simplify file listing.

--- templates/api-file-page.tpl.php
+++ templates/api-file-page.tpl.php
@@ -17,10 +17,7 @@
  * - $file->summary: Short summary.
  * - $file->documentation: HTML formatted comments.
  * - $file->code: HTML formatted source code.
- * - $file->classes: List of classes, if any.
- * - $file->constants: List of constants, if any.
- * - $file->globals: List of globals, if any.
- * - $file->functions: List of functions, if any.
+ * - $file->objects: Documented objects HTML.
  * - $file->see: Related api objects.
  */
 ?>
@@ -35,27 +32,4 @@
 <?php print $see ?>
 <?php } ?>
 
-<?php if (!empty($interfaces)) { ?>
-<h3><?php print t('Interfaces') ?></h3>
-<?php print $interfaces ?>
-<?php } ?>
-<?php if (!empty($classes)) { ?>
-<h3><?php print t('Classes') ?></h3>
-<?php print $classes ?>
-<?php } ?>
-<?php if (!empty($constants)) { ?>
-<h3><?php print t('Constants') ?></h3>
-<?php print $constants ?>
-<?php } ?>
-<?php if (!empty($globals)) { ?>
-<h3><?php print t('Globals') ?></h3>
-<?php print $globals ?>
-<?php } ?>
-<?php if (!empty($functions)) { ?>
-<h3><?php print t('Functions') ?></h3>
-<?php print $functions ?>
-<?php } ?>
-<?php if (!empty($related_topics)) { ?>
-<h3><?php print t('Related topics') ?></h3>
-<?php print $related_topics ?>
-<?php } ?>
+<?php print $objects; ?>
