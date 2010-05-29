8efee4ac87b36c1ee566ec2e3b65b795dee7a1ae
Neil Drumm <drumm@3064.no-reply.drupal.org>
2010-05-29 23:41:32 +0000

    Simplify topic listing.

--- templates/api-group-page.tpl.php
+++ templates/api-group-page.tpl.php
@@ -22,21 +22,6 @@
 <?php print $see ?>
 <?php } ?>
 
-<?php if (!empty($constants)) { ?>
-<h3><?php print t('Constants') ?></h3>
-<?php print $constants ?>
-<?php } ?>
-<?php if (!empty($globals)) { ?>
-<h3><?php print t('Globals') ?></h3>
-<?php print $globals ?>
-<?php } ?>
-<?php if (!empty($functions)) { ?>
-<h3><?php print t('Functions') ?></h3>
-<?php print $functions ?>
-<?php } ?>
-<?php if (!empty($files)) { ?>
-<h3><?php print t('Files') ?></h3>
-<?php print $files ?>
-<?php } ?>
+<?php print $objects; ?>
 
 <?php print $defined; ?>
