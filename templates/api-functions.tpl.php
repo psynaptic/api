25c150b0fb23e4628e433d54d279f717a88e3ce9
Richard Burford <rich@freestylesystems.co.uk>
2012-01-20 18:06:55 +0000

    Issue #1411776 by psynaptic: Relegated lists of function calls and hook implementations to separate pages.

--- templates/api-functions.tpl.php
+++ templates/api-functions.tpl.php
@@ -5,11 +5,13 @@
  * Theme implementation to show a list of functions, their source and description.
  *
  * Available variables:
+ * - $title: The title for the page e.g., "N functions call function()".
  * - $function['function']: Function link.
  * - $function['file']: File link.
  * - $function['description']: Function description.
  */
 ?>
+<h2><?php print $title ?></h2>
 <dl class="api-functions">
 <?php foreach ($functions as $function) { ?>
   <dt><?php print $function['function'] ?> <small>in <?php print $function['file'] ?></small></dt>
