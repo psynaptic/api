c0672dd3d7043c27493eb9b01892493a06e6932d
Richard Burford <rich@freestylesystems.co.uk>
2012-01-20 18:06:55 +0000

    Issue #1411776 by psynaptic: Relegated lists of function calls and hook implementations to separate pages.

---
+++ templates/api-function-reference-page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file api-function-reference-page.tpl.php
+ * Theme implementation to display a list of functions that call or implement
+ * this function.
+ *
+ * Available variables:
+ * - $functions: Themed list of functions that call or implement this function.
+ * - $is_admin: True or false.
+ * - $logged_in: True or false.
+ */
+?>
+<?php print $functions ?>
