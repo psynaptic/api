fd1fc77905a82805a1a555027ee5dd213040d257
Richard Burford <rich@freestylesystems.co.uk>
2012-01-20 18:10:09 +0000

    Initial work to remove autocomplete.

--- api.js
+++ api.js
@@ -1,24 +1,24 @@
-Drupal.behaviors.apiAutoComplete = function (context) {
-  $('#api-search-form:not(.apiAutoCompleteProcessed)', context).addClass('apiAutoCompleteProcessed').each(function () {
-    // On the first focus.
-    $('#edit-search', this).attr('autocomplete', 'off').one('focus', function () {
-      var $this = $(this);
-      // Prefetch list of objects for this branch.
-      $.getJSON(Drupal.settings.apiAutoCompletePath, function (data) {
-        // Attach to autocomplete.
-        $this.autocomplete(data, {
-          sort: function (a, b) {
-            return a.value.length - b.value.length;
-          },
-          matchContains: true,
-          max: 200,
-          scroll: true,
-          scrollHeight: 360,
-          width: 300
-        }).result(function () {
-          $this.get(0).form.submit();
-        }).focus();
-      });
-    });
-  });
-};
+//Drupal.behaviors.apiAutoComplete = function (context) {
+//  $('#api-search-form:not(.apiAutoCompleteProcessed)', context).addClass('apiAutoCompleteProcessed').each(function () {
+//    // On the first focus.
+//    $('#edit-search', this).attr('autocomplete', 'off').one('focus', function () {
+//      var $this = $(this);
+//      // Prefetch list of objects for this branch.
+//      $.getJSON(Drupal.settings.apiAutoCompletePath, function (data) {
+//        // Attach to autocomplete.
+//        $this.autocomplete(data, {
+//          sort: function (a, b) {
+//            return a.value.length - b.value.length;
+//          },
+//          matchContains: true,
+//          max: 200,
+//          scroll: true,
+//          scrollHeight: 360,
+//          width: 300
+//        }).result(function () {
+//          $this.get(0).form.submit();
+//        }).focus();
+//      });
+//    });
+//  });
+//};
