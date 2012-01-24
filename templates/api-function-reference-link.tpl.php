df4234f2e535061c76a2e203dbbbc3c1b8c91751
Richard Burford <rich@freestylesystems.co.uk>
2012-01-24 15:34:01 +0000

    Added missing file.

---
+++ templates/api-function-reference-link.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file api-function-reference-link.tpl.php
+ * Theme implementation for the function reference link on function pages.
+ *
+ * Available variables:
+ * - $title: The title of the link e.g., "N functions call function()".
+ * - $path: The path of the link to the reference list page.
+ * - $link: The fully formed link to the reference list page.
+ */
+?>
+<h3><?php print $link ?></h3>
